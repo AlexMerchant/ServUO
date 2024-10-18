44760a884079a62294f1c75e908f244dd622d0d7
Alex Merchant <alex.m.merchant92@gmail.com>
2024-10-17 23:40:15 -0500

    NEW ITEM (WIP)
    * Early stages of an item that allows staff assume an alias and toggle
        on/off AccessLevel.Player
    * Currently the item doesn't serial; need to set up SER/DESER writers

---
+++ Scripts/Custom Scripts/AchillesStone.cs
@@ -0,0 +1,82 @@
+using System;
+using Server.Mobiles;
+
+namespace Server.Items
+{
+    public class AchillesStone : Item
+    {
+        private bool m_IsOwned;
+        private Mobile m_Owner;
+        private AccessLevel m_OwnerAccessLevel;
+
+        [CommandProperty(AccessLevel.Owner)]
+        public bool IsOwned
+        {
+            get { return m_IsOwned; }
+            set
+            {
+                m_IsOwned = value;
+            }
+        }
+
+        [CommandProperty(AccessLevel.Owner)]
+        public Mobile Owner
+        {
+            get { return m_Owner; }
+            set
+            {
+                m_Owner = value;
+            }
+        }
+
+        [CommandProperty(AccessLevel.Owner)]
+        public AccessLevel OwnerAccessLevel
+        {
+            get { return m_OwnerAccessLevel; }
+            set { m_OwnerAccessLevel = value; }
+        }
+
+        [Constructable]
+        public AchillesStone()
+            : base(0x3192) // ItemID: 12690
+        {
+            Name = "a stone of mortality";
+            IsOwned = false;
+        }
+
+        [Constructable]
+        public AchillesStone(Serial serial)
+            : base(serial)
+        {
+
+        }
+
+        public override void OnDoubleClick(Mobile from)
+        {
+            from.SendMessage("Serial: {0} / Mobile: {1}", from.Serial.Value, from);
+
+            // Checks if the AchillesStone already has an owner
+            if (!IsOwned && !(Owner != null))
+            {
+                this.Owner = from;
+                this.OwnerAccessLevel = from.AccessLevel;
+                this.IsOwned = true;
+                from.SendMessage("Owner set");
+            }
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0); // version
+        }
+    }
+}
